6011232d3192071b4134163ded3fc31b9f347be5
Jade Clarke <45100065+jade-clarke@users.noreply.github.com>
2024-02-14 08:50:43 -0500

    fixed functions path

--- vite.server.config.js
+++ vite.server.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         ssr: 'src/functions/submitForm.js',
-        outDir: 'dist/functions', 
+        outDir: 'functions', 
         rollupOptions: { 
             // Adjust 'external' as needed depending on your libraries    
             external: ['@sendgrid/mail'],
